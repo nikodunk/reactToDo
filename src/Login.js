11f8d8a45d3615482c1042740196a63b5db6ba1b
nikodunk <n.dunkel@gmail.com>
2017-04-30 13:59:44 -0400

    dnd removed

--- src/Login.js
+++ src/Login.js
@@ -1,35 +1,32 @@
 import React from 'react';
 
 export default class Login extends React.Component {
-  
+
   render() {
 
     return (
             <div>
-              { !this.props.username ? <h1>Hi! What's your name?</ h1> : null}
-              { this.props.username ? <h1>Hi, {this.props.username}!</ h1> : null}
+              { !this.props.username ? <h1>Hi! What's your name?</h1> : null}
+              { this.props.username ? <h1>Hi, {this.props.username}!</h1> : null}
               <form>
                 <input
-                  autoFocus 
+                  autoFocus
                   type="text"
                   className="addItemInput"
                   onSubmit={this.props.handleUsernameSubmit}
-                  onChange={this.props.handleUsernameChange('username')} 
+                  onChange={this.props.handleUsernameChange('username')}
                   value={this.props.username}
 
                 />
-                <input 
+                <input
                   style={{color:"lightgreen"}}
                   className="submitButton"
-                  type="submit" 
-                  value="Let's Go!" 
+                  type="submit"
+                  value="Let's Go!"
                   onClick={this.props.handleUsernameSubmit} />
               </form>
-            </div> 
+            </div>
 
     );
   }
 }
-
-
-
--- src/TodoList.js
+++ src/TodoList.js
@@ -13,9 +13,6 @@ var config = {
 
 firebase.initializeApp(config);
 
-var placeholder = document.createElement("div");
-placeholder.className = "placeholder";
-
 
 export default class TodoList extends React.Component {
 
@@ -25,11 +22,8 @@ export default class TodoList extends React.Component {
     this.handleRemove = this.handleRemove.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.dragOver = this.dragOver.bind(this);
-    this.dragStart = this.dragStart.bind(this);
-    this.dragEnd = this.dragEnd.bind(this);
     this.state = {
-      name: '', 
+      name: '',
       address: '',
       uid: '',
       items: [],
@@ -42,8 +36,8 @@ export default class TodoList extends React.Component {
     this.items = [{}];
     this.ref = firebase.database().ref(this.state.username);
     this.ref.on("child_added", function(dataSnapshot) {
-      this.items.push( dataSnapshot.val()); 
-      this.setState({ 
+      this.items.push( dataSnapshot.val());
+      this.setState({
           items: this.items.slice(1,100)
       })
     }.bind(this));
@@ -72,7 +66,7 @@ export default class TodoList extends React.Component {
         address: this.state.address,
         uid: this.state.uid
       });
-    }  
+    }
 
   handleRemove (key) {
     return function () {
@@ -87,63 +81,6 @@ export default class TodoList extends React.Component {
       }.bind(this);
   }
 
-  dragStart(e) {
-    this.dragged = e.currentTarget;
-    e.dataTransfer.effectAllowed = 'move';
-    console.log('za')
-    // Firefox requires calling dataTransfer.setData
-    // for the drag to properly work
-    e.dataTransfer.setData("text/html", e.currentTarget);
-  }
-
-  dragEnd(e) {
-    this.dragged.style.display = "block";
-    this.dragged.parentNode.removeChild(placeholder);
-
-    // Update state
-    var newData = this.state.items;
-    var from = Number(this.dragged.dataset.id);
-    var to = Number(this.over.dataset.id);
-    
-    if(from < to) to--;
-    if(this.nodePlacement == "after") to++;
-
-    newData.splice(to, 0, newData.splice(from, 1)[0]);
-    this.setState({items: newData});
-
-    this.ref = firebase.database().ref(this.state.username);
-    this.ref.set(this.state.items);
-
-  }
-  
-  dragOver(e) {
-    e.preventDefault();
-
-
-    this.dragged.style.display = "none";
-    if(e.target.className == "placeholder") return;
-    this.over = e.target;
-
-
-
-    // Inside the dragOver method
-    var relY = e.clientY - this.over.offsetTop;
-    var height = this.over.offsetHeight / 2;
-    var parent = e.target.parentNode;
-
-    if(relY > height) {
-      this.nodePlacement = "after";
-      parent.insertBefore(placeholder, e.target.nextElementSibling);
-    }
-    else if(relY < height) {
-      this.nodePlacement = "before"
-      parent.insertBefore(placeholder, e.target);
-    }
-
-
-    
-  }
-
 
   render() {
     return (
@@ -155,15 +92,12 @@ export default class TodoList extends React.Component {
               <button onClick={this.props.handleLogout} className="logoutButton">¡Adios!</button>
             </div>
 
-            <div onDragOver={this.dragOver} style={{marginTop:50}}>
+            <div style={{marginTop:50}}>
               {this.state.items.map((item, i) =>
                   <div
                     className="todoListItem"
                     data-id={i}
                     key={i}
-                    draggable="true"
-                    onDragEnd={this.dragEnd}
-                    onDragStart={this.dragStart}
                   > <b>{item.name}</b> &nbsp; {item.address}
                   <button
                         className="removeButton"
@@ -171,22 +105,20 @@ export default class TodoList extends React.Component {
                         onClick={this.handleRemove(i)}
                         >
                         Remove
-                    </button>  
+                    </button>
+                    <hr />
                   </div>
               )}
             </div>
 
             <TodoListForm
-              name={this.state.name} 
-              address={this.state.address} 
-              handleSubmit={this.handleSubmit} 
-              handleChange={this.handleChange} 
+              name={this.state.name}
+              address={this.state.address}
+              handleSubmit={this.handleSubmit}
+              handleChange={this.handleChange}
             />
-        </div> 
+        </div>
     );
   }
 
 }
-
-
-
